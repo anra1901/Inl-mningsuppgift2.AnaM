ac7e0453e39ac2e35c69a617a21e08ee7f6dc9b3
Ana Maria Ramirez <anra1901>
2019-11-04 17:17:41 +0100

    Finished without styling

--- main.js
+++ main.js
@@ -1,7 +1,7 @@
 const listOfCountryContinent = [
-    { country: "France", continent: "Europe" },
-    { country: "Singapore", continent: "Asia" },
-    { country: "Paraguay", continent: "South America" }
+    { country: "France", date: "2019-10-01", continent: "Europe" },
+    { country: "Singapore", date: "2019-12-08", continent: "Asia" },
+    { country: "Paraguay", date: "2019-12-15", continent: "South America" }
 ]
 const country = document.querySelector("#country")
 const filter = document.querySelector("#filter")
@@ -14,34 +14,12 @@ let filteredCountries = []
 let filteredContinents = []
 let showAllContinents = true
 
-
-
-add.addEventListener("submit", event => {
-    event.preventDefault()
-    if (country.value !== "") {
-        if (continentIsSelected()) {
-            listOfCountryContinent.push({ country: country.value, continent: option.value })
-        }
-    } else {
-        alert("Please insert a country!")
-    }
-    country.value = ""
-    option.value = "selectContinent"
-    const radioBtns = document.getElementsByName("cathegory")
-        for (i = 0; i < radioBtns.length; i++) {
-            if (radioBtns[i].value === "all") {
-                radioBtns[i].checked = true
-            }
-        }
-    drawList(listOfCountryContinent)
-})
-
-const continentIsSelected = () => {
-    if (option.value === "selectContinent") {
-        alert("Select a continent from the list!")
-        return false
-    }
-    return true
+const todayCheck = () => {
+    let date = new Date()
+    let year = date.getFullYear()
+    let month = date.getMonth()
+    let day = date.getDate()
+    return new Date(year, month, day, 0, 0, 0, 0)
 }
 
 const drawList = arr => {
@@ -51,6 +29,13 @@ const drawList = arr => {
         const countrySpan = document.createElement("span")
         countrySpan.className = "countrySpan"
         countrySpan.textContent = obj.country
+        const dateSpan = document.createElement("span")
+        if (new Date(obj.date) < todayCheck()) {
+            dateSpan.className = "expiredDate"
+        } else {
+            dateSpan.className = "dateSpan"
+        }
+        dateSpan.textContent = obj.date
         const continentSpan = document.createElement("span")
         continentSpan.className = "continentSpan"
         continentSpan.textContent = obj.continent
@@ -61,6 +46,7 @@ const drawList = arr => {
             removeCountry(obj)
         })
         li.appendChild(countrySpan)
+        li.appendChild(dateSpan)
         li.appendChild(continentSpan)
         li.appendChild(removeBtn)
         countryList.appendChild(li)
@@ -68,10 +54,40 @@ const drawList = arr => {
 }
 drawList(listOfCountryContinent)
 
+add.addEventListener("submit", event => {
+    event.preventDefault()
+    if (country.value !== "") {
+        if (continentIsSelected()) {
+            listOfCountryContinent.push({ country: country.value, date: theDate.value, continent: option.value })
+        }
+    } else {
+        alert("Please insert a country!")
+    }
+    resetValues()
+    drawList(listOfCountryContinent)
+})
+
+const todayDate = () => {
+    let date = new Date()
+    const dateString = date.toISOString().slice(0, date.toISOString().indexOf("T"))
+    document.getElementById("theDate").value = dateString
+}
+todayDate()
+
+
+const continentIsSelected = () => {
+    if (option.value === "selectContinent") {
+        alert("Select a continent from the list!")
+        return false
+    }
+    return true
+}
+
 const removeCountry = x => {
     const index = listOfCountryContinent.indexOf(x)
     listOfCountryContinent.splice(index, 1)
     drawList(listOfCountryContinent)
+    resetValues()
 }
 
 filter.addEventListener("input", event => {
@@ -101,3 +117,20 @@ const filterByContinent = event => {
     }
 
 }
+
+const resetRadioBtns = () => {
+    const radioBtns = document.getElementsByName("cathegory");
+    for (i = 0; i < radioBtns.length; i++) {
+        if (radioBtns[i].value === "all") {
+            radioBtns[i].checked = true;
+        }
+    }
+}
+
+const resetValues = () => {
+    country.value = ""
+    option.value = "selectContinent"
+    todayDate()
+    resetRadioBtns()
+}
+
